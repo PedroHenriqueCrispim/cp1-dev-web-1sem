23d05bf21a3854c0ca57680f4c1d1531e080c117
pedrohenriquecrispim <teixerahac@gmail.com>
2023-04-26 17:24:00 -0300

    cp1-dev-web-1sem

--- script.js
+++ script.js
@@ -28,7 +28,13 @@ function checkAnswers() {
     let answer1 = document.getElementById("answer1").value.toLowerCase();
     let answer2 = document.getElementById("answer2").value.toLowerCase();
     let answer3 = document.getElementById("answer3").value.toLowerCase();
-
+	let answer4 = document.getElementById("answer3").value.toLowerCase();
+	let answer5 = document.getElementById("answer3").value.toLowerCase();
+	let answer6 = document.getElementById("answer3").value.toLowerCase();
+	let answer7 = document.getElementById("answer3").value.toLowerCase();
+	let answer8 = document.getElementById("answer3").value.toLowerCase();
+	let answer9 = document.getElementById("answer3").value.toLowerCase();
+	let answer10 = document.getElementById("answer3").value.toLowerCase();
     let result = "";
 
     if (answer1 === "chardonnay") {
@@ -49,53 +55,47 @@ function checkAnswers() {
         result += "Resposta incorreta para a pergunta 3. A região produtora de vinho mais famosa da França é Bordeaux.<br>";
     }
 
-	if (ansewer4 === "Carbonatação") {
+	if (answer4 === "Carbonatação") {
 		result += "Resposta correta para a pergunta 4!<br>";
 	} else {
 		result += "Resposta incorreta para a pergunta 4. O processo pelo qual as bolhas são adicionadas ao vinho é o Carbonatação.<br>";
 	}
 
-	if (ansewer5 === " Tanino") {
+	if (answer5 === " Tanino") {
 		result += "Resposta correta para a pergunta 5!<br>";
 	} else {
 		result += "Resposta incorreta para a pergunta 5. O nome da substância que causa o sabor amargo em alguns vinhos é o Tanino. <br>";
 	}
 
-	if (ansewer6 === "Éster") {
+	if (answer6 === "Éster") {
 		result += "Resposta correta para a pergunta 6!<br>";
 	} else {
 		result += "Resposta incorreta para a pergunta 6. O nome da substância que causa o sabor frutado em alguns vinhos é Éster. <br>";
 	}
 
-	if (ansewer7 === "Enófilo") {
+	if (answer7 === "Enófilo") {
 		result += "Resposta correta para a pergunta 7!<br>";
 	} else {
 		result += "Resposta incorreta para a pergunta 7. O nome dado à degustação de vinho para avaliar a qualidade é Enófilo. <br>";
 	}
 
-	if (ansewer8 === "Barrica") {
+	if (answer8 === "Barrica") {
 		result += "Resposta correta para a pergunta 8!<br>";
 	} else {
 		result += "Resposta incorreta para a pergunta 8. O nome dado ao recipiente onde o vinho é envelhecido é Barrica <br>";
 	}
 
-	if (ansewer9 === "Passito") {
+	if (answer9 === "Passito") {
 		result += "Resposta correta para a pergunta 9!<br>";
 	} else {
 		result += "Resposta incorreta para a pergunta 9. O nome dado ao vinho feito a partir de uvas maduras colhidas tardiamente é Passito <br>";
 	}
 
-	if (ansewer10 === "GSM") {
+	if (answer10 === "GSM") {
 		result += "Resposta correta para a pergunta 10!<br>";
 	} else {
 		result += "Resposta incorreta para a pergunta 10. O nome dado ao vinho tinto feito a partir de uvas Grenache, Syrah e Mourvèdre é GSM <br>";
 	}
-
-
-
-
-
-
 
     document.getElementById("result").innerHTML = result;
 }
