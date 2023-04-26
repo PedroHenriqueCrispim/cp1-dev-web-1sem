b7d420687ca97079779fbab25ab3033c09e4ff7f
pedrohenriquecrispim <teixerahac@gmail.com>
2023-04-26 16:44:55 -0300

    inicio do quiz

--- script.js
+++ script.js
@@ -23,75 +23,31 @@ function quiz() {
 
 /*quiz.html*/
 
-const questions = [
-	{
-		question: "Qual é a uva mais plantada no mundo?",
-		choices: ["Chardonnay", "Merlot", "Cabernet Sauvignon", "Pinot Noir"],
-		answer: "Chardonnay"
-	},
-	{
-		question: "Qual é a diferença entre um vinho tinto e um vinho branco?",
-		choices: ["A cor", "O sabor", "A região de origem", "O tipo de uva"],
-		answer: "A cor"
-	},
-	{
-		question: "Qual é a região produtora de vinho mais famosa da França?",
-		choices: ["Bordeaux", "Burgundy", "Rhone", "Loire"],
-		answer: "Bordeaux"
-	}
-];
 
-let currentQuestion = 0;
-let score = 0;
+function checkAnswers() {
+    let answer1 = document.getElementById("answer1").value.toLowerCase();
+    let answer2 = document.getElementById("answer2").value.toLowerCase();
+    let answer3 = document.getElementById("answer3").value.toLowerCase();
 
-const questionElement = document.getElementById("question");
-const choicesElement = document.getElementById("choices");
-const submitButton = document.getElementById("submit");
-const scoreElement = document.getElementById("score");
-const restartButton = document.getElementById("restart");
+    let result = "";
 
-function showQuestion() {
-	const question = questions[currentQuestion];
-	questionElement.innerText = question.question;
+    if (answer1 === "chardonnay") {
+        result += "Resposta correta para a pergunta 1!<br>";
+    } else {
+        result += "Resposta incorreta para a pergunta 1. A uva mais plantada do mundo é a Chardonnay.<br>";
+    }
 
-	choicesElement.innerHTML = "";
-	for (let i = 0; i < question.choices.length; i++) {
-		const choice = question.choices[i];
-		const label = document.createElement("label");
-		const radio = document.createElement("input");
-		radio.type = "radio";
-		radio.name = "choice";
-		radio.value = choice;
-		label.appendChild(radio);
-		label.appendChild(document.createTextNode(choice));
-		choicesElement.appendChild(label);
-	}
-}
+    if (answer2 === "cor") {
+        result += "Resposta correta para a pergunta 2!<br>";
+    } else {
+        result += "Resposta incorreta para a pergunta 2. A diferença entre um vinho tinto e um vinho branco é a cor.<br>";
+    }
 
-function submitAnswer() {
-	const selectedChoice = document.querySelector('input[name="choice"]:checked');
-	if (!selectedChoice) {
-		return;
-	}
-	
-	if (selectedChoice.value === questions[currentQuestion].answer) {
-		score++;
-	}
-	
-	currentQuestion++;
-	
-	if (currentQuestion === questions.length) {
-		showScore();
-	} else {
-		showQuestion();
-	}
-}
+    if (answer3 === "bordeaux") {
+        result += "Resposta correta para a pergunta 3!<br>";
+    } else {
+        result += "Resposta incorreta para a pergunta 3. A região produtora de vinho mais famosa da França é Bordeaux.<br>";
+    }
 
-function showScore() {
-	questionElement.innerText = `Você acertou ${score} de ${questions.length} perguntas!`;
-	choicesElement.style.display = "none";
-	submitButton.style.display = "none";
-	scoreElement.style.display = "block";
-	scoreElement.innerText = `Sua pontuação é ${score} de ${questions.length}`;
-	restartButton.style.display = "block";
+    document.getElementById("result").innerHTML = result;
 }
