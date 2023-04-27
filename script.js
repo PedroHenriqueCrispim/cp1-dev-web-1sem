5e06f88009c38855d2edaa250b1e9d37596431c2
pedrohenriquecrispim <teixerahac@gmail.com>
2023-04-27 12:09:26 -0300

    site pronto

--- script.js
+++ script.js
@@ -8,18 +8,6 @@ function changeBackground() {
 
 setInterval(changeBackground, 3000);
 
-function quiz() {
-	var pontuacao = 0;
-
-	var resposta1 = prompt("Qual é a uva mais plantada no mundo?");
-	if (resposta1.toLowerCase() == "cabernet sauvignon") {
-		pontuacao += 1;
-	}
-
-	// Continuar com as outras perguntas do quiz...
-
-	alert("Sua pontuação final é " + pontuacao + ".");
-}
 
 /*quiz.html*/
 
@@ -39,69 +27,69 @@ function checkAnswers() {
 
 
     if (answer1 === "chardonnay") {
-        result += "Resposta correta para a pergunta 1!<br>";
+        result += "Resposta correta para a pergunta 1!<br><br>";
     } else {
-        result += "Resposta incorreta para a pergunta 1. A uva mais plantada do mundo é a Chardonnay.<br>";
+        result += "Resposta incorreta para a pergunta 1. A uva mais plantada do mundo é a Chardonnay.<br><br>";
     }
 
     if (answer2 === "a cor") {
-        result += "Resposta correta para a pergunta 2!<br>";
+        result += "Resposta correta para a pergunta 2!<br><br>";
 		
     } 
 	else if (answer2 === "cor"){
-		result += "Resposta correta para a pergunta 2!<br>";
+		result += "Resposta correta para a pergunta 2!<br><br>";
 	}
 
 	else {
-        result += "Resposta incorreta para a pergunta 2. A diferença entre um vinho tinto e um vinho branco é a cor.<br>";
+        result += "Resposta incorreta para a pergunta 2. A diferença entre um vinho tinto e um vinho branco é a cor.<br><br>";
     }
 
     if (answer3 === "bordeaux") {
-        result += "Resposta correta para a pergunta 3!<br>";
+        result += "Resposta correta para a pergunta 3!<br><br>";
     } else {
-        result += "Resposta incorreta para a pergunta 3. A região produtora de vinho mais famosa da França é Bordeaux.<br>";
+        result += "Resposta incorreta para a pergunta 3. A região produtora de vinho mais famosa da França é Bordeaux.<br><br>";
     }
 
 	if (answer4 === "Carbonatação") {
-		result += "Resposta correta para a pergunta 4!<br>";
+		result += "Resposta correta para a pergunta 4!<br><br>";
 	} else {
-		result += "Resposta incorreta para a pergunta 4. O processo pelo qual as bolhas são adicionadas ao vinho é o Carbonatação.<br>";
+		result += "Resposta incorreta para a pergunta 4. O processo pelo qual as bolhas são adicionadas ao vinho é o Carbonatação.<br><br>";
 	}
 
 	if (answer5 === " Tanino") {
-		result += "Resposta correta para a pergunta 5!<br>";
+		result += "Resposta correta para a pergunta 5!<br><br>";
 	} else {
-		result += "Resposta incorreta para a pergunta 5. O nome da substância que causa o sabor amargo em alguns vinhos é o Tanino. <br>";
+		result += "Resposta incorreta para a pergunta 5. O nome da substância que causa o sabor amargo em alguns vinhos é o Tanino. <br><br>";
 	}
 
 	if (answer6 === "Éster") {
-		result += "Resposta correta para a pergunta 6!<br>";
+		result += "Resposta correta para a pergunta 6!<br><br>";
 	} else {
-		result += "Resposta incorreta para a pergunta 6. O nome da substância que causa o sabor frutado em alguns vinhos é Éster. <br>";
+		result += "Resposta incorreta para a pergunta 6. O nome da substância que causa o sabor frutado em alguns vinhos é Éster. <br><br>";
 	}
 
 	if (answer7 === "Enófilo") {
-		result += "Resposta correta para a pergunta 7!<br>";
+		result += "Resposta correta para a pergunta 7!<br><br>";
 	} else {
-		result += "Resposta incorreta para a pergunta 7. O nome dado à degustação de vinho para avaliar a qualidade é Enófilo. <br>";
+		result += "Resposta incorreta para a pergunta 7. O nome dado à degustação de vinho para avaliar a qualidade é Enófilo. <br><br>";
 	}
 
 	if (answer8 === "Barrica") {
-		result += "Resposta correta para a pergunta 8!<br>";
+		result += "Resposta correta para a pergunta 8!<br><br>";
 	} else {
-		result += "Resposta incorreta para a pergunta 8. O nome dado ao recipiente onde o vinho é envelhecido é Barrica <br>";
+		result += "Resposta incorreta para a pergunta 8. O nome dado ao recipiente onde o vinho é envelhecido é Barrica <br><br>";
 	}
 
 	if (answer9 === "Passito") {
-		result += "Resposta correta para a pergunta 9!<br>";
+		result += "Resposta correta para a pergunta 9!<br><br>";
 	} else {
-		result += "Resposta incorreta para a pergunta 9. O nome dado ao vinho feito a partir de uvas maduras colhidas tardiamente é Passito <br>";
+		result += "Resposta incorreta para a pergunta 9. O nome dado ao vinho feito a partir de uvas maduras colhidas tardiamente é Passito <br><br>";
 	}
 
 	if (answer10 === "GSM") {
-		result += "Resposta correta para a pergunta 10!<br>";
+		result += "Resposta correta para a pergunta 10!<br><br>";
 	} else {
-		result += "Resposta incorreta para a pergunta 10. O nome dado ao vinho tinto feito a partir de uvas Grenache, Syrah e Mourvèdre é GSM <br>";
+		result += "Resposta incorreta para a pergunta 10. O nome dado ao vinho tinto feito a partir de uvas Grenache, Syrah e Mourvèdre é GSM <br><br>";
 	}
 
     document.getElementById("result").innerHTML = result;
